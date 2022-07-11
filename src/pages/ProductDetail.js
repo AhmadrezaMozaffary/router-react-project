cb0317dbbab6b9b56f62ae5721180beb16fb3b9e
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-07-11 08:16:00 +0430

    Nested route

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,10 +1,11 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ProductDetail = () => {
   const { productID } = useParams();
   return (
     <section>
       <p>Product detail for ID : {productID}</p>
+      <Link to="/products">&larr;</Link>
     </section>
   );
 };
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,13 @@
+import { Link, Route } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1> Welcome page</h1>;
+  return (
+    <section>
+      <h1> Welcome page</h1>
+      <p>Are you new user ? {<Link to="/welcome/new-user">Click Me</Link>}</p>
+      <Route path="/welcome/new-user">🎉Welcome new user</Route>
+    </section>
+  );
 };
 
 export default Welcome;
